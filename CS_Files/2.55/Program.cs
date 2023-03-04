9e28b50621a195d5d1fa57ad5d5396e852a2797f
KucherovPV <118096557+KucherovPV@users.noreply.github.com>
2023-03-04 11:55:08 +0700

    Update Program.cs

--- CS_Files/2.55/Program.cs
+++ CS_Files/2.55/Program.cs
@@ -1,24 +1,10 @@
-﻿using System;
-using System.IO;
 using Newtonsoft.Json;
 using System.Net;
-using WeatherApp;
-using System.Collections.Generic;
+
+namespace WeatherApp;
+
 public class Program
 {
-    public static void SaveRightsCategory()
-    {
-        string path = "C:\\Users\\gr623_kupvi\\Desktop\\2.5 files\\Rights_category.txt";
-
-        using (StreamWriter writer = new StreamWriter(path, false))
-        {
-            while (reader.Read())
-            {
-                writer.WriteLine(
-                    $"Id: {reader[0]} Категория: {reader[1]}");
-            }
-        }   
-    }
     public static void CheckTheWeather(string city)
     {
         //Console.WriteLine("Введите название города");
@@ -31,28 +17,51 @@ public class Program
         StreamReader read = new StreamReader(res.GetResponseStream());
         string response = read.ReadToEnd();
         WeatherResponse weatherNow = JsonConvert.DeserializeObject<WeatherResponse>(response);
- 
-        Console.WriteLine($"Погода в городе на сегодная: {weatherNow.Name}\n" +
-                          $"Температура: {weatherNow.Main.Temp}°C\n" +
-                          $"Ощущается как: {weatherNow.Main.Feels_like}°C\n" +
-                          $"Максимальная температура в пределах города: {weatherNow.Main.Temp_max}°C\n" +
-                          $"Минимальная температура в пределах города: {weatherNow.Main.Temp_mine}°C\n" +
-                          $"Погодные условия: {weatherNow.Weather[0].Main}\n" +
-                          $"Дополнительные сведения о погодных условиях: {weatherNow.Weather[0].Description}\n" +
-                          $"Скорость ветра: {weatherNow.Wind.Speed} М/С\n");
-
-        CheckUnuql obj = new CheckUnuql(weatherNow.Name,weatherNow.Main.Temp,);
+        var ress = $"Погода в городе: {weatherNow.Name}\n" +
+                   $"Температура: {weatherNow.Main.Temp}°C\n" +
+                   $"Ощущается как: {weatherNow.Main.Feels_like}°C\n" +
+                   $"Максимальная температура в пределах города: {weatherNow.Main.Temp_max}°C\n" +
+                   $"Минимальная температура в пределах города: {weatherNow.Main.Temp_mine}°C\n" +
+                   $"Погодные условия: {weatherNow.Weather[0].Main}\n" +
+                   $"Дополнительные сведения о погодных условиях: {weatherNow.Weather[0].Description}\n" +
+                   $"Скорость ветра: {weatherNow.Wind.Speed} М/С\n";
+        
+        string path = "C:\\Users\\pavel\\Desktop\\UserRequest.txt";
+        
+        using (StreamWriter writer = new StreamWriter(path, true))
+        {
+            writer.WriteLine(ress);
+        }
     }
-    
+
+    public static void ShowRequest()
+    {
+        string path = "C:\\Users\\pavel\\Desktop\\UserRequest.txt";
+        using (StreamReader reader = new StreamReader(path))
+        {
+            string? line;
+            while ((line =  reader.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
 
     public static void Main()
     {
         while (true)
         {
-            Console.WriteLine("Введите название города");
+            Console.WriteLine("Введите название города, \"exit\" для выхода, \"Show\" для показа истории запросов");
             string? city = Console.ReadLine();
             if (city == "Exit") break;
-            CheckTheWeather(city);
+            switch (city)
+            {
+                case "Show":
+                    ShowRequest();
+                    break;
+                default: CheckTheWeather(city);
+                    break;
+            }
         }
     }
 }
